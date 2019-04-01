c617bbd6dfad198f82a59a143efcd72662ad0bba
zissalg <thai.cuong1404@gmail.com>
2019-04-01 10:28:02 +0700

    add program argv

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 import proxy
 import log
+import sys
 
-proxy = proxy.Proxy('127.0.0.1', 8889)
+port = 8888
+
+if (len(sys.argv) == 1):
+    print('Port argv is empty, use default port 8888')
+elif (len(sys.argv) == 2):
+    port = int(sys.argv[1])
+
+print('Listening on port: ', port)
+proxy = proxy.Proxy('127.0.0.1', port)
 proxy.start()
 proxy.close()
 log.close()
